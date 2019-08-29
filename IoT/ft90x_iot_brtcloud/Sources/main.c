95d91151d2b7ecb1c28c0a1eda20537dd09e3edb
srikanth <srikanth.anandapu@brtchip.com>
2019-08-29 11:41:38 +0800

    updated for recipient details.

--- IoT/ft90x_iot_brtcloud/Sources/main.c
+++ IoT/ft90x_iot_brtcloud/Sources/main.c
@@ -64,7 +64,7 @@
 ///////////////////////////////////////////////////////////////////////////////////
 #define CONFIG_NOTIFICATION_UART_KEYWORD "Hello World"
 #define CONFIG_NOTIFICATION_RECIPIENT "srikanth.anandapu@brtchip.com"
-#define CONFIG_NOTIFICATION_RECIPIENT_NUMBER "+6581946347"
+#define CONFIG_NOTIFICATION_RECIPIENT_NUMBER "+1234567890"
 #define CONFIG_NOTIFICATION_MESSAGE "Hi, How are you today?"
 ///////////////////////////////////////////////////////////////////////////////////
 
